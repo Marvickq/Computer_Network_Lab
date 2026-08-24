5fb7e5c0f87aa358edaf38da530cd03528bcb9d6
test <test>
2026-08-24 11:57:30 +0530

    lab 4

--- lab3_1.py
+++ lab3_1.py
@@ -1,173 +1,274 @@
-def subnetting(ip_address, required_subnets):
+import math
 
-    # Split IP address
-    octets = list(map(int, ip_address.split(".")))
 
-    # Determine default prefix based on IP class
-    if octets[0] <= 127:
-        default_prefix = 8
-    elif octets[0] <= 191:
-        default_prefix = 16
+# ------------------------------------------------------------
+# Convert IP address to 32-bit integer
+# ------------------------------------------------------------
+def ip_to_int(ip):
+    parts = ip.split(".")
+    result = 0
+
+    for part in parts:
+        result = (result << 8) | int(part)
+
+    return result
+
+
+# ------------------------------------------------------------
+# Convert 32-bit integer to IP address
+# ------------------------------------------------------------
+def int_to_ip(ip):
+    return (
+        str((ip >> 24) & 255) + "." +
+        str((ip >> 16) & 255) + "." +
+        str((ip >> 8) & 255) + "." +
+        str(ip & 255)
+    )
+
+
+# ------------------------------------------------------------
+# Determine IP Class
+# ------------------------------------------------------------
+def get_class(first_octet):
+
+    if 1 <= first_octet <= 126:
+        return "A"
+
+    elif 128 <= first_octet <= 191:
+        return "B"
+
+    elif 192 <= first_octet <= 223:
+        return "C"
+
+    elif 224 <= first_octet <= 239:
+        return "D"
+
     else:
-        default_prefix = 24
+        return "E"
 
-    # --------------------------------
-    # Calculate borrowed bits
-    # --------------------------------
+
+# ------------------------------------------------------------
+# Get Default Prefix
+# ------------------------------------------------------------
+def get_default_prefix(ip_class):
+
+    if ip_class == "A":
+        return 8
+
+    elif ip_class == "B":
+        return 16
+
+    elif ip_class == "C":
+        return 24
+
+    else:
+        return 0
+
+
+# ------------------------------------------------------------
+# Convert Prefix to Subnet Mask
+# ------------------------------------------------------------
+def prefix_to_mask(prefix):
+
+    if prefix == 0:
+        return 0
+
+    return (0xFFFFFFFF << (32 - prefix)) & 0xFFFFFFFF
+
+
+# ------------------------------------------------------------
+# SUBNETTING FUNCTION
+# ------------------------------------------------------------
+def subnetting(ip, required_subnets):
+
+    # Convert IP to integer
+    ip_int = ip_to_int(ip)
+
+    # Find first octet
+    first_octet = (ip_int >> 24) & 255
+
+    # Find IP class
+    ip_class = get_class(first_octet)
+
+    # Class D and E cannot be used for normal subnetting
+    if ip_class == "D" or ip_class == "E":
+
+        print("\nClass", ip_class,
+              "does not support normal subnetting.")
+
+        return
+
+    # Get default prefix
+    default_prefix = get_default_prefix(ip_class)
+
+    # --------------------------------------------------------
+    # Find number of bits to borrow
+    # --------------------------------------------------------
 
     borrowed_bits = 0
-    number_of_subnets = 1
 
-    while number_of_subnets < required_subnets:
+    while (2 ** borrowed_bits) < required_subnets:
         borrowed_bits += 1
-        number_of_subnets = 2 ** borrowed_bits
 
     # New prefix
     new_prefix = default_prefix + borrowed_bits
 
-    # Check if prefix is valid
+    # Maximum practical subnetting for this program
     if new_prefix > 30:
-        print("Error: Too many subnets. Not enough host bits.")
+
+        print("\nToo many subnets requested for this IP class.")
+
         return
 
-    # Host bits remaining
+    # --------------------------------------------------------
+    # Calculate masks
+    # --------------------------------------------------------
+
+    default_mask = prefix_to_mask(default_prefix)
+    new_mask = prefix_to_mask(new_prefix)
+
+    # --------------------------------------------------------
+    # Number of actual subnets
+    # --------------------------------------------------------
+
+    total_subnets = 2 ** borrowed_bits
+
+    # --------------------------------------------------------
+    # Hosts per subnet
+    # --------------------------------------------------------
+
     host_bits = 32 - new_prefix
 
-    # Hosts per subnet
     hosts_per_subnet = (2 ** host_bits) - 2
 
-    # --------------------------------
-    # Calculate subnet mask
-    # --------------------------------
+    # Total usable hosts
+    total_hosts = total_subnets * hosts_per_subnet
 
-    mask = [0, 0, 0, 0]
+    # --------------------------------------------------------
+    # Calculate network address
+    # --------------------------------------------------------
 
-    full_octets = new_prefix // 8
-    remaining_bits = new_prefix % 8
+    network_address = ip_int & default_mask
 
-    for i in range(full_octets):
-        mask[i] = 255
+    # --------------------------------------------------------
+    # Calculate entire network range
+    # --------------------------------------------------------
 
-    if remaining_bits > 0:
-        mask[full_octets] = 256 - (2 ** (8 - remaining_bits))
+    first_ip = network_address
 
-    # --------------------------------
-    # Find subnetting octet
-    # --------------------------------
+    last_ip = network_address + (2 ** (32 - default_prefix)) - 1
 
-    if remaining_bits == 0:
-        subnet_octet = full_octets - 1
-    else:
-        subnet_octet = full_octets
+    # --------------------------------------------------------
+    # OUTPUT
+    # --------------------------------------------------------
 
-    block_size = 256 - mask[subnet_octet]
+    print("\nIP Address :", ip)
 
-    # --------------------------------
-    # Find network address
-    # --------------------------------
-
-    network = octets.copy()
-
-    for i in range(subnet_octet + 1, 4):
-        network[i] = 0
-
-    network[subnet_octet] = (
-        (network[subnet_octet] // block_size) * block_size
-    )
-
-    # --------------------------------
-    # Display result
-    # --------------------------------
-
-    print("\n========== SUBNETTING RESULT ==========")
-
-    print("IP Address        :", ip_address)
-    print("Default Prefix    : /", default_prefix)
-    print("Required Subnets  :", required_subnets)
-    print("Borrowed Bits     :", borrowed_bits)
-    print("New Prefix        : /", new_prefix)
-    print("Subnet Mask       :", ".".join(map(str, mask)))
-    print("Number of Subnets :", number_of_subnets)
-    print("Hosts per Subnet  :", hosts_per_subnet)
-    print("Block Size        :", block_size)
-
-    # --------------------------------
-    # Subnet table
-    # --------------------------------
-
-    print("\n========== SUBNET TABLE ==========")
+    print("IP Class   : Class", ip_class)
 
     print(
-        f"{'Subnet':<18}"
+        "Default Subnet :",
+        int_to_ip(default_mask),
+        "/" + str(default_prefix)
+    )
+
+    print(
+        "New Subnet Mask :",
+        int_to_ip(new_mask),
+        "/" + str(new_prefix)
+    )
+
+    print("Bits Borrowed :", borrowed_bits)
+
+    # --------------------------------------------------------
+    # IP ADDRESS RANGE
+    # --------------------------------------------------------
+
+    print("\n--------------------------------------------")
+    print("IP ADDRESS RANGE")
+    print("--------------------------------------------")
+
+    print("Network Address :", int_to_ip(first_ip))
+
+    print("First IP        :", int_to_ip(first_ip + 1))
+
+    print("Last IP         :", int_to_ip(last_ip - 1))
+
+    print("Broadcast Address:", int_to_ip(last_ip))
+
+    # --------------------------------------------------------
+    # SUBNET INFORMATION
+    # --------------------------------------------------------
+
+    print("\n--------------------------------------------")
+    print("SUBNET INFORMATION")
+    print("--------------------------------------------")
+
+    print("Required Subnets :", required_subnets)
+
+    print("Actual Subnets   :", total_subnets)
+
+    print("Hosts per Subnet :", hosts_per_subnet)
+
+    print("Total Usable Hosts:", total_hosts)
+
+    # --------------------------------------------------------
+    # LIST ALL SUBNETS
+    # --------------------------------------------------------
+
+    print("\n--------------------------------------------")
+    print("LIST OF ALL SUBNETS")
+    print("--------------------------------------------")
+
+    subnet_size = 2 ** host_bits
+
+    print(
+        f"{'Subnet':<10}"
+        f"{'Network':<18}"
         f"{'First Host':<18}"
         f"{'Last Host':<18}"
         f"{'Broadcast':<18}"
     )
 
-    print("-" * 72)
+    print("-" * 82)
 
-    # Convert IP address to integer
-    network_int = (
-        (network[0] << 24)
-        | (network[1] << 16)
-        | (network[2] << 8)
-        | network[3]
-    )
+    for i in range(total_subnets):
 
-    subnet_size = 2 ** host_bits
+        # Network address of subnet
+        subnet_network = network_address + (i * subnet_size)
 
-    # --------------------------------
-    # Convert integer back to IP
-    # --------------------------------
+        # Broadcast address
+        subnet_broadcast = subnet_network + subnet_size - 1
 
-    def int_to_ip(value):
+        # First host
+        first_host = subnet_network + 1
 
-        return ".".join([
-            str((value >> 24) & 255),
-            str((value >> 16) & 255),
-            str((value >> 8) & 255),
-            str(value & 255)
-        ])
-
-    # --------------------------------
-    # Generate subnet table
-    # --------------------------------
-
-    for subnet_number in range(number_of_subnets):
-
-        subnet_int = network_int + (subnet_number * subnet_size)
-
-        broadcast_int = subnet_int + subnet_size - 1
-
-        first_host_int = subnet_int + 1
-
-        last_host_int = broadcast_int - 1
-
-        subnet_address = int_to_ip(subnet_int)
-        first_host = int_to_ip(first_host_int)
-        last_host = int_to_ip(last_host_int)
-        broadcast = int_to_ip(broadcast_int)
+        # Last host
+        last_host = subnet_broadcast - 1
 
         print(
-            f"{subnet_address:<18}"
-            f"{first_host:<18}"
-            f"{last_host:<18}"
-            f"{broadcast:<18}"
+            f"{i + 1:<10}"
+            f"{int_to_ip(subnet_network):<18}"
+            f"{int_to_ip(first_host):<18}"
+            f"{int_to_ip(last_host):<18}"
+            f"{int_to_ip(subnet_broadcast):<18}"
         )
 
 
-# --------------------------------
+# ------------------------------------------------------------
 # MAIN PROGRAM
-# --------------------------------
+# ------------------------------------------------------------
 
-ip = input("Enter IP address: ")
+ip = input("Enter IP Address: ")
 
-subnets = int(input("Enter required number of subnets: "))
+required_subnets = int(
+    input("Enter required number of subnets: ")
+)
 
-if subnets <= 0:
+if required_subnets <= 0:
 
     print("Error: Number of subnets must be greater than 0.")
 
 else:
 
-    subnetting(ip, subnets)
+    subnetting(ip, required_subnets)
